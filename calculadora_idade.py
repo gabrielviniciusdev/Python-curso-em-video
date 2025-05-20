248af6dedd6e25b199e55a6862eb480817d21ec2
gabrielviniciusdev <gv7303175@gmail.com>
2025-05-20 14:24:43 -0300

    Add files via upload
    Adicionando exercício aula 7 - calculadora de idade

---
+++ calculadora_idade.py
@@ -0,0 +1,7 @@
+nome = input("Qual é o seu nome? ")
+ano_nasc = int(input("Em que ano você nasceu? "))
+ano_atual = 2025
+idade = ano_atual - ano_nasc
+
+print(f"Olá, {nome}! Em {ano_atual}, você terá {idade} anos.")
+ 
